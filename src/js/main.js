ffa0418cf7170fdafd669654a819d7f32ff2f4ed
Marta <marta.venturacoelho@studentfr.ch>
2025-02-05 17:11:31 +0100

    js final

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,12 @@ var header = document.querySelector(".l-header");
 
 card.forEach((card) => {
   card.addEventListener("click", () => {
-    var hiddenText = document.querySelector(".product-hidden-text");
+    var hiddenText = card.querySelector(".product-hidden-text");
     hiddenText.classList.toggle("is-active");
     card.classList.toggle("is-active");
   });
 });
 
-function yellow() {
+header.addEventListener("click", () => {
   header.classList.toggle("is-active");
-}
-
-header.addEventListener("click", yellow);
+});
